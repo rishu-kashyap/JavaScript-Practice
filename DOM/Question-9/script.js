a2a8c658b4ac1ac003d67fe607615ffa38321e53
Rishu kumar <risikjha@gmail.com>
2026-06-04 02:04:09 +0530

    Counting the word you enter in the placeholder

---
+++ DOM/Question-9/script.js
@@ -0,0 +1,8 @@
+var txtarea = document.querySelector("textarea");
+var counter = document.querySelector("#counter");
+
+txtarea.addEventListener("input", function(){
+    counter.textContent = txtarea.value.length;
+})
+
+
