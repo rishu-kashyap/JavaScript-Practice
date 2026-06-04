ef4cdc946aa2e266152f8acf844c1170330a7fc3
Rishu kumar <risikjha@gmail.com>
2026-06-05 04:37:29 +0530

    Learned GSAP Baics and timeline animations

---
+++ GSAP/Tutorial_1_basics/script.js
@@ -0,0 +1,17 @@
+// gsap.to("#box",{
+//     x:1000,
+//     rotate:360,
+//     backgroundColor:"blue",
+//     duration:2,
+//     delay:1
+
+// })
+
+gsap.from("#box",{
+    x:1000,
+    rotate:360,
+    backgroundColor:"blue",
+    duration:2,
+    delay:1
+
+})
